d4d6672f8d5758678b9cc5b642e7cb15b77a9fc4
Vladimir Ilimurzin <ilimurzin@ya.ru>
2025-05-07 15:55:13 +0300

    Счетчик дней до Нового Года

--- app/src/main/java/ru/ilimurzin/daysuntilnewyear/MainActivity.kt
+++ app/src/main/java/ru/ilimurzin/daysuntilnewyear/MainActivity.kt
@@ -4,24 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import ru.ilimurzin.daysuntilnewyear.ui.theme.DaysUntilNewYearTheme
+import androidx.compose.ui.unit.sp
+import ru.ilimurzin.daysuntilnewyear.ui.theme.Theme
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DaysUntilNewYearTheme {
+            Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Counter(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +36,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Counter(modifier: Modifier = Modifier) {
+    val today = LocalDate.now()
+    val newYear = LocalDate.of(today.year + 1, 1, 1) // Next January 1st
+    val daysUntilNewYear = ChronoUnit.DAYS.between(today, newYear)
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = daysUntilNewYear.toString(),
+            fontSize = 48.sp,
+        )
+        Text(
+            text = "days until New Year",
+            fontSize = 24.sp,
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    DaysUntilNewYearTheme {
-        Greeting("Android")
+    Theme {
+        Counter()
     }
 }
