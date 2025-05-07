fcda279ead19d0850f35d0ec6a99273ef6f1405a
Vladimir Ilimurzin <ilimurzin@ya.ru>
2025-05-07 19:13:37 +0300

    Сколько минут до Нового Года, сколько секунд и наступление Нового Года

--- app/src/main/java/ru/ilimurzin/daysuntilnewyear/MainActivity.kt
+++ app/src/main/java/ru/ilimurzin/daysuntilnewyear/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : ComponentActivity() {
             Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Counter(
+                        now = LocalDateTime.now(),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -36,50 +37,149 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Counter(modifier: Modifier = Modifier) {
-    val now = LocalDateTime.now()
+fun Counter(
+    now: LocalDateTime,
+    modifier: Modifier = Modifier,
+) {
     val newYear = LocalDateTime.of(now.year + 1, 1, 1, 0, 0) // Next January 1st 00:00
     val daysUntilNewYear = ChronoUnit.DAYS.between(now, newYear)
 
-    if (daysUntilNewYear > 0) {
-        Column(
+    if (daysUntilNewYear == 365L) {
+        return Column(
             modifier = modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Text(
-                text = daysUntilNewYear.toString(),
+                text = "New Year!",
                 fontSize = 48.sp,
             )
-            Text(
-                text = "days until New Year",
-                fontSize = 24.sp,
-            )
         }
+    }
+
+    val number: String
+    val description: String
+
+    if (daysUntilNewYear > 0) {
+        number = daysUntilNewYear.toString()
+        description = if (daysUntilNewYear == 1L) "day" else "days"
     } else {
         val hoursUntilNewYear = ChronoUnit.HOURS.between(now, newYear)
 
-        Column(
-            modifier = modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Text(
-                text = hoursUntilNewYear.toString(),
-                fontSize = 48.sp,
-            )
-            Text(
-                text = "hours until New Year",
-                fontSize = 24.sp,
-            )
+        if (hoursUntilNewYear > 0) {
+            number = hoursUntilNewYear.toString()
+            description = if (hoursUntilNewYear == 1L) "hour" else "hours"
+        } else {
+            val minutesUntilNewYear = ChronoUnit.MINUTES.between(now, newYear)
+
+            if (minutesUntilNewYear > 0) {
+                number = minutesUntilNewYear.toString()
+                description = if (minutesUntilNewYear == 1L) "minute" else "minutes"
+            } else {
+                val secondsUntilNewYear = ChronoUnit.SECONDS.between(now, newYear)
+
+                number = secondsUntilNewYear.toString()
+                description = if (secondsUntilNewYear == 1L) "second" else "seconds"
+            }
         }
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = number,
+            fontSize = 48.sp,
+        )
+        Text(
+            text = description,
+            fontSize = 24.sp,
+        )
+        Text(
+            text = " until New Year",
+            fontSize = 24.sp,
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun NowPreview() {
     Theme {
-        Counter()
+        Counter(LocalDateTime.now())
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TwoDaysBeforePreview() {
+    Theme {
+        Counter(LocalDateTime.of(LocalDateTime.now().year, 12, 29, 23, 0))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OneDayBeforePreview() {
+    Theme {
+        Counter(LocalDateTime.of(LocalDateTime.now().year, 12, 30, 23, 0))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TwoHoursBeforePreview() {
+    Theme {
+        Counter(LocalDateTime.of(LocalDateTime.now().year, 12, 31, 22, 0))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OneHourBeforePreview() {
+    Theme {
+        Counter(LocalDateTime.of(LocalDateTime.now().year, 12, 31, 23, 0))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TwoMinutesBeforePreview() {
+    Theme {
+        Counter(LocalDateTime.of(LocalDateTime.now().year, 12, 31, 23, 58))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OneMinuteBeforePreview() {
+    Theme {
+        Counter(LocalDateTime.of(LocalDateTime.now().year, 12, 31, 23, 59))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TwoSecondsBeforePreview() {
+    Theme {
+        Counter(LocalDateTime.of(LocalDateTime.now().year, 12, 31, 23, 59, 58))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OneSecondBeforePreview() {
+    Theme {
+        Counter(LocalDateTime.of(LocalDateTime.now().year, 12, 31, 23, 59, 59))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NewYearPreview() {
+    Theme {
+        Counter(LocalDateTime.of(LocalDateTime.now().year, 1, 1, 0, 0))
     }
 }
